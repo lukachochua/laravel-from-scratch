3b4b8b1fe1d14a82c715a3a971b85f5db48d77bb
lukachochua <lukachochua@gmail.com>
2022-07-20 01:22:23 +0400

    feat: create first route and link it

---
+++ resources/views/post.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Blog</title>
+    <link rel="stylesheet" href="/app.css">
+</head>
+<body>
+    <article>
+        <h1><a href="/post">My First Post</a></h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Reiciendis sed cumque provident dolorum atque consequuntur voluptate eaque sit labore saepe delectus aperiam, 
+            magni corrupti voluptas perspiciatis possimus non praesentium dicta? Voluptas consequatur porro hic, facilis odit neque totam nulla cumque nihil aliquam! Repellendus
+             alias sequi, ut similique tempora modi ducimus voluptatem, eius suscipit tenetur non quia. Natus voluptate unde ea dolore voluptates officiis quisquam ad repellat. 
+             Sequi error illum ullam obcaecati a quae dolore. Quia animi adipisci ratione laborum excepturi maxime pariatur omnis libero voluptatibus molestiae beatae ipsam porro 
+             neque quaerat tempora, perspiciatis possimus officia! Quod facere voluptatem vitae sit.
+        </p>
+    </article>
+    
+    <a href="/">Go Back</a>
+
+</body>
+</html>
