f40660f590bc7ab63f5598e222333f9ccc25894d
lukachochua <lukachochua@gmail.com>
2022-07-23 16:31:43 +0400

    add Yaml Front matter to retrieve post metadata

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,24 +2,50 @@
 
 namespace App\Models;
 use Illuminate\Support\Facades\File;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
 
 
 class Post 
 {
+    public $title;
+
+    public $excerpt;
+
+    public $date;
+
+    public $body;
+
+    public $slug;
+
+    public function __construct($title, $excerpt, $date, $body, $slug)
+    {
+        $this->title = $title;
+        $this->excerpt = $excerpt;
+        $this->date = $date;
+        $this->body = $body;
+        $this->slug = $slug;
+    }
+
     public static function find($slug) 
     {
-        if (! file_exists($path = resource_path("posts/{$slug}.html"))) {
-            throw new ModelNotFoundException();
-        }
-
-        return cache()->remember("posts.{$slug}", 1200, fn() => file_get_contents($path));
+        return static::all()->firstWhere('slug', $slug);
     }
 
 
     public static function all()
     {
-        $files = File::files(resource_path("posts/"));
-
-        return array_map(fn ($file) => $file->getContents(), $files);
+        return collect(File::files(resource_path("posts/")))
+        ->map(function ($file){
+            return YamlFrontMatter::parseFile($file);
+        })
+        ->map(function ($document){
+            return new Post(
+                $document->title,
+                $document->excerpt,
+                $document->date,
+                $document->body(),
+                $document->slug
+            );
+        });
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
+
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,6 +15,7 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
+
     return view('posts', [
         'posts' => Post::all()
     ]);
