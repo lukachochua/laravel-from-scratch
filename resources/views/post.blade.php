8bcdf6f5794a2f87bce2b9f89a1abf7a2388d94e
lukachochua <lukachochua@gmail.com>
2022-07-24 18:24:57 +0400

    Do not touch eol and go to blade

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -9,9 +9,11 @@
 </head>
 <body>
     <article>
-            <h1><?= $post->title; ?></h1>
+            <h1>{{ $post->title }}</h1>
 
-            <div><?= $post->body; ?></div>
+            <div>
+                {!! $post->body !!}
+            </div>
     </article>
     
     <a href="/">Go Back</a>
