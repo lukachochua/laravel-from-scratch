57e55299078ae161d78081394da3f258f5eab46a
lukachochua <lukachochua@gmail.com>
2022-07-31 00:10:25 +0400

    feat: add clockwork to check querys

--- routes/web.php
+++ routes/web.php
@@ -17,7 +17,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
 
     return view('posts', [
-        'posts' => Post::all()
+        'posts' => Post::with('category')->get()
     ]);
 });
 
@@ -30,8 +30,6 @@ Route::get('posts/{post:slug}', function (Post $post) {
 });
 
 Route::get('categories/{category:slug}', function (Category $category) {
-
-
     return view('posts', [
         'posts' => $category->posts
     ]);
