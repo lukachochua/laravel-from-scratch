3260f168896dce5c49d1f195030088a74fa90c06
lukachochua <lukachochua@gmail.com>
2022-08-13 02:27:35 +0400

    feat: implement validation and let old data unchanged if validation fails

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
 class RegisterController extends Controller
 {
@@ -17,8 +18,8 @@ class RegisterController extends Controller
 
         $attributes = request()->validate([
             'name' => 'required|max:255',
-            'username' => 'required|max:255|min:3',
-            'email' => 'required|email|max:255',
+            'username' => ['required','max:255', 'min:3', Rule::unique('users', 'username')],
+            'email' => ['required','max:255', 'min:3', Rule::unique('users', 'email')],
             'password' => 'required|max:255|min:7'
         ]);
 
--- resources/views/register/create.blade.php
+++ resources/views/register/create.blade.php
@@ -16,8 +16,13 @@
                             type="text"
                             name="name" 
                             id="name" 
+                            value="{{ old('name')}}"
                             required
                     >
+
+                    @error('name')
+                        <p class="text-red-500 text-xs mt1"> {{ $message }}</p>                        
+                    @enderror
                 </div>
                 
 
@@ -32,8 +37,12 @@
                             type="text"
                             name="username" 
                             id="username" 
+                            value="{{ old('username')}}"
                             required
                     >
+                    @error('username')
+                        <p class="text-red-500 text-xs mt1"> {{ $message }}</p>                        
+                    @enderror                    
                 </div>
 
                 <div class="mb-6">
@@ -47,8 +56,13 @@
                             type="email"
                             name="email" 
                             id="email" 
+                            value="{{ old('email')}}"
                             required
                     >
+
+                    @error('email')
+                        <p class="text-red-500 text-xs mt1"> {{ $message }}</p>                        
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -64,6 +78,10 @@
                             id="password" 
                             required
                     >
+
+                    @error('password')
+                        <p class="text-red-500 text-xs mt1"> {{ $message }}</p>                        
+                    @enderror
                 </div>
 
                 <div class="mb-6">
@@ -74,6 +92,14 @@
                     </button>
                 </div>
                 
+                {{-- @if ($errors->any())
+                    <ul>
+                        @foreach ($errors->all() as $error)
+                            <li class="text-red-500 text-xs">{{ $error }}</li>
+                        @endforeach
+                    </ul>
+                @endif --}}
+
             </form>
         </main>
     </section>
