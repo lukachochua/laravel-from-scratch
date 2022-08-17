7bd3b00ba890d6f588832260d5601841c336e65a
lukachochua <lukachochua@gmail.com>
2022-08-17 21:45:39 +0400

    feat: create service container, interface for mailing and registration binding

---
+++ app/Services/ConvertKitNewsletter.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Services;
+
+use MailchimpMarketing\ApiClient;
+
+class MailchimpNewsletter implements Newsletter
+{
+
+    public function subscribe(string $email, string $list = null)
+    {
+
+    }
+}
--- app/Services/Newsletter.php
+++ app/Services/Newsletter.php
@@ -2,22 +2,7 @@
 
 namespace App\Services;
 
-use MailchimpMarketing\ApiClient;
-
-class Newsletter
+interface Newsletter
 {
-    public function subscribe(string $email)
-    {
-        $mailchimp = new ApiClient();
-
-        $mailchimp->setConfig([
-            'apiKey' => config('services.mailchimp.key'),
-            'server' => 'us12'
-        ]);
-
-        return $mailchimp->lists->AddListMember('e57712d324', [
-            'email_address' => $email,
-            'status' => 'subscribed'
-        ]);
-    }
+    public function subscribe(string $email, string $list = null);
 }
